a82232dd7d6a0ae4f7e20a5c3acb01996190c643
Ankur Oberoi <aoberoi@gmail.com>
2014-04-01 03:18:31 -0400

    randomly generates new rooms, implements hello world in them

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var opentok = require('opentok'),
     express = require('express'),
+    Moniker = require('moniker'),
     apiKey = process.env.API_KEY,
     apiSecret = process.env.API_SECRET,
     ot = opentok(apiKey, apiSecret),
@@ -7,11 +8,37 @@ var opentok = require('opentok'),
 
 app.use(express.static(__dirname+'/public'));
 
+var rooms = {};
+
 app.get('/', function(req, res) {
-  res.send('hello world');
+  var roomName;
+  do {
+    roomName = Moniker.choose()
+  } while(roomName in rooms)
+  res.redirect('/'+roomName);
 });
 
-var port = Number(process.env.PORT || 5000);
+app.get('/:roomName', function(req, res) {
+  var roomReady = function(err, sessionId) {
+    if (err) throw err;
+    var token = ot.generateToken(sessionId);
+    res.render('chat.ejs', { apiKey: apiKey, sessionId: sessionId, token: token });
+  }
+
+  if (rooms[req.params.roomName]) {
+    setImmediate(function() {
+      roomReady(null, rooms[req.params.roomName].sessionId);
+    });
+  } else {
+    ot.createSession(function(err, session) {
+      if (err) return roomReady(err);
+      rooms[req.params.roomName] = { sessionId : session.sessionId };
+      roomReady(null, session.sessionId);
+    });
+  }
+});
+
+var port = Number(process.env.PORT || 3000);
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
